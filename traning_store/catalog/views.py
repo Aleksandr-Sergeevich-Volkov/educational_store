0295487ebcd6703410438ae8a892e0c32d23850d
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2024-10-09 18:00:04 +0300

    commit №5

--- traning_store/catalog/views.py
+++ traning_store/catalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
-# Create your views here.
+class ProductListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Product
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10 
+    template_name = 'product_list.html'
--- traning_store/homepage/views.py
+++ traning_store/homepage/views.py
@@ -5,4 +5,8 @@ class HomePage(TemplateView):
     # В атрибуте template_name обязательно указывается имя шаблона,
     # на основе которого будет создана возвращаемая страница.
     template_name = 'index.html' 
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomePage.self).get_context_data(*args, **kwargs)
+        context['catalog'] = 'catalog/'
+        return context
      

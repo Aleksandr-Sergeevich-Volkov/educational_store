c2dcfaa67364c91a1bfd94e6cfd2522e80723392
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-04-03 23:16:30 +0300

    commit write settings

--- traning_store/traning_store/settings.py
+++ traning_store/traning_store/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = 'django-insecure-4#xre@vo3c)grje)5kj3rrwt6@1=' \
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0','62.113.36.63'
+]
 INTERNAL_IPS = ['127.0.0.1']
 
 

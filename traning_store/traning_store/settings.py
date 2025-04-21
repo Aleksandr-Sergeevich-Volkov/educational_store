7f855c8a1fc5665fc0f921891dd30191d8b92795
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-04-21 23:26:59 +0300

    commit write nginx.conf

--- traning_store/traning_store/settings.py
+++ traning_store/traning_store/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-4#xre@vo3c)grje)5kj3rrwt6@1=' \
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0','62.113.36.63'
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0','62.113.36.63','4505485-volkovaleksandr.twc1.net'
 ]
 INTERNAL_IPS = ['127.0.0.1']
 

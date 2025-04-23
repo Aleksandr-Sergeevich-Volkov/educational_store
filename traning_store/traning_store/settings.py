4464489d438eef2b00d9407bd0774ea63336f7d0
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-04-23 16:27:15 +0300

    commit write base.html

--- traning_store/traning_store/settings.py
+++ traning_store/traning_store/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-4#xre@vo3c)grje)5kj3rrwt6@1=' \
              '8dvyc!@6m-6nzl!kkb9dvm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0','62.113.36.63','4505485-volkovaleksandr.twc1.net'
 ]

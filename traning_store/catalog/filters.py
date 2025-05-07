043b8702f4835d3146d78ec94acd3c9c9609685c
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-05-07 16:56:44 +0300

    commit write *.html

--- traning_store/catalog/filters.py
+++ traning_store/catalog/filters.py
@@ -4,7 +4,9 @@ from .models import Product
 
 
 class ProductFilter(django_filters.FilterSet):
-
+    Appoitment = django_filters.CharFilter(field_name='Appoitment', lookup_expr='exact', label='Назначение')
+    Sock = django_filters.CharFilter(field_name='Sock', lookup_expr='exact', label='Тип носка')
+    
     class Meta:
         model = Product
         fields = ['Appointment', 'Sock']

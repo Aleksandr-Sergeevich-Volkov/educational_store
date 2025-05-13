1b86d0c70767d6c9631a5186aacfe520e6ca5751
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-05-13 21:46:17 +0300

    commit write filters

--- traning_store/catalog/filters.py
+++ traning_store/catalog/filters.py
@@ -4,11 +4,7 @@ from .models import Appointment, Product, Soсk
 
 
 class ProductFilter(django_filters.FilterSet):
-    # Appoitnment = django_filters.CharFilter(field_name='Appoitment', lookup_expr='exact', label='Назначение')
-    # Appoitnment = django_filters.ChoiceFilter(choices=Product.Appointment, label='Назначение')
     Appoitnment = django_filters.ModelChoiceFilter(queryset=Appointment.objects.all(), label='Назначение')
-
-    # Sock = django_filters.CharFilter(field_name='Sock', lookup_expr='exact', label='Тип носка')
     Sock = django_filters.ModelChoiceFilter(queryset=Soсk.objects.all(), label='Тип носка')
 
     class Meta:

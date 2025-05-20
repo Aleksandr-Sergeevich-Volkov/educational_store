8cc4a25e2e2b265106ecbdfdd10f73b07114961b
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-05-20 23:08:28 +0300

    Flake8

--- traning_store/orders/forms.py
+++ traning_store/orders/forms.py
@@ -1,9 +1,9 @@
 import logging
 
 from django import forms
+from django.db import models
 
 from .models import Order
-from django.db import models
 
 logger = logging.getLogger(__name__)
 
@@ -11,11 +11,12 @@ logger = logging.getLogger(__name__)
 class OrderCreateForm(forms.ModelForm):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField('Фамилия', max_length=20)
-    email = models.EmailField('Почта',max_length=300)
+    email = models.EmailField('Почта', max_length=300)
     address = models.CharField('Адрес', max_length=20)
     address_pvz = models.CharField('Адрес ПВЗ', max_length=20)
     postal_code = models.CharField('Индекс', max_length=20)
     city = models.CharField('Город', max_length=20)
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'address_pvz',

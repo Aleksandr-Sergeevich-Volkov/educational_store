fd47260b8bd4e118effa735eaaa668546d960d97
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-06-02 12:49:23 +0300

    robokassa

--- traning_store/traning_store/urls.py
+++ traning_store/traning_store/urls.py
@@ -7,7 +7,7 @@ from django.urls import include, path, re_path, reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 
-from . import robokassa, views
+from . import views
 
 urlpatterns = [
     re_path(r'^coupons/', include(('coupons.urls', 'coupons'),
@@ -29,7 +29,7 @@ urlpatterns = [
         name='registration',
     ),
     path('fail/', TemplateView.as_view(template_name='fail.html')),
-    path('robokassa/result/', robokassa.result_payment),
+    path('robokassa/result/', views.result_payment),
     path('admin/', admin.site.urls),
 ]
 # Подключаем дебаг-панель:

9996763dc3893bac0386209ff652a6d3a69f7113
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-06-21 23:40:42 +0300

    add yandex.html

--- traning_store/homepage/urls.py
+++ traning_store/homepage/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -6,5 +7,6 @@ app_name = 'homepage'
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='homepage'),
-    path('<int:pk>/', views.detail_view, name='detail')
+    path('<int:pk>/', views.detail_view, name='detail'),
+    path('yandex_445ca9b51fd08dec.html/', TemplateView.as_view(template_name='yandex_445ca9b51fd08dec.html')),
 ]

bca2a85e7c07085b70acbee1897cb29e4a911563
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-07-12 23:08:52 +0300

    add delivety_app

--- traning_store/homepage/views.py
+++ traning_store/homepage/views.py
@@ -7,8 +7,6 @@ from .models import Post
 
 
 class HomePage(TemplateView):
-    # В атрибуте template_name обязательно указывается имя шаблона,
-    # на основе которого будет создана возвращаемая страница.
     template_name = 'index.html'
 
     def get_context_data(self, *args, **kwargs):

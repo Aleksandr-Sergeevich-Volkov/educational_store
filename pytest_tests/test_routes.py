63dea2ce58aa87079a9f81a4555992ddd29873ca
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-08-13 22:05:14 +0300

    add django test

---
+++ pytest_tests/test_routes.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+import pytest
+from django.urls import reverse
+
+
+# Указываем в фикстурах встроенный клиент.
+@pytest.mark.django_db
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('homepage:homepage')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK

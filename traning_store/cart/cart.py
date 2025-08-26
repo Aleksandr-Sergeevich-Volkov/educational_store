23896a9c4b84fb01e25465eba210137b2a7cd06e
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-08-26 23:11:46 +0300

    add test cart

--- traning_store/cart/cart.py
+++ traning_store/cart/cart.py
@@ -36,7 +36,8 @@ class Cart(object):
     def add(self, product, quantity=1, size='1', color='черный',
             m_type='Стандартный', images_m='1', update_quantity=False):
         # Добавить продукт в корзину или обновить его количество.
-        product_id = str(product.id)
+        # product_id = str(product.id)
+        product_id = product.id
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,
                                      'price': str(product.price),

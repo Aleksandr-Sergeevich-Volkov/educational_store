2a4a352c8f71a6816a2d47548c60b8ec803d5948
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-08-29 23:17:12 +0300

    add pytest

--- pytest/test_example.py
+++ pytest/test_example.py
@@ -2,7 +2,10 @@ from http import HTTPStatus
 
 from django.urls import reverse
 
+import pytest
 
+
+@pytest.mark.django_db
 def test_home_page(client):
     url = reverse('homepage:homepage')
     response = client.get(url)

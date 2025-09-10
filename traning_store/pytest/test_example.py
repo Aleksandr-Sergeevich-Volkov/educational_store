f586e5be49e4569f6e2e82d73f359a67413f5415
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-09-10 14:51:37 +0300

    default articul,code

--- traning_store/pytest/test_example.py
+++ traning_store/pytest/test_example.py
@@ -1,7 +1,17 @@
+from http import HTTPStatus
+
 import pytest
 from django.core.management import call_command
+from django.urls import reverse
 
 
 @pytest.fixture
 def data():
     call_command('loaddata', 'db.json', verbosity=0)
+
+
+@pytest.mark.django_db
+def test_home_page(client):
+    url = reverse('homepage:homepage')
+    response = client.get(url)
+    assert response.status_code, HTTPStatus.OK

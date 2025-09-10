b8eac084e9cdb66f0afbbc4c0949cec915dcd18f
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-09-10 15:08:27 +0300

    default articul,code

--- traning_store/pytest/test_example.py
+++ traning_store/pytest/test_example.py
@@ -1,9 +1,10 @@
 from http import HTTPStatus
 
 import pytest
+from cart.cart import Cart
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.management import call_command
-from django.test import RequestFactory
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
 
@@ -25,3 +26,23 @@ def test_home_page(client):
     url = reverse('homepage:homepage')
     response = client.get(url)
     assert response.status_code, HTTPStatus.OK
+
+
+class TestCart(TestCase):
+    @pytest.fixture
+    @pytest.mark.django_db
+    def cart_session(self):
+        self.request = RequestFactory().get('/')
+        middleware = SessionMiddleware(get_response=lambda r: None)
+        middleware.process_request(self.request)
+        self.request.session.save()
+
+    @pytest.mark.django_db
+    def test_initialize_cart_clean_session(self):
+        self.request = RequestFactory().get('/')
+        middleware = SessionMiddleware(get_response=lambda r: None)
+        middleware.process_request(self.request)
+        self.request.session.save()
+        request = self.request
+        cart = Cart(request)
+        assert cart.cart == {}

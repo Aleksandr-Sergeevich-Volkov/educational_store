e9484d58c78e08395f625ff37118b5cff6094a3b
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-09-10 15:18:12 +0300

    default articul,code

--- traning_store/pytest/test_example.py
+++ traning_store/pytest/test_example.py
@@ -2,8 +2,10 @@ from http import HTTPStatus
 
 import pytest
 from cart.cart import Cart
+from catalog.models import Color, Gallery, Model_type, Product, Size
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.management import call_command
+from django.shortcuts import get_object_or_404
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
@@ -46,3 +48,28 @@ class TestCart(TestCase):
         request = self.request
         cart = Cart(request)
         assert cart.cart == {}
+
+    @pytest.mark.django_db
+    @pytest.mark.usefixtures('data', 'cart_session')
+    def test_add_cart(self):
+        request = self.request
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=1)
+        color = get_object_or_404(Color, id=1)
+        size = get_object_or_404(Size, id=1)
+        model_type = get_object_or_404(Model_type, id=1)
+        images_m = Gallery.objects.filter(product=product)
+        cart.add(product=product,
+                 quantity=1,
+                 size=size,
+                 color=color,
+                 m_type=model_type,
+                 images_m=images_m,)
+        test_cart = {'color': 'Черный',
+                     'images_m': '<QuerySet [<Gallery: Gallery object (1)>, <Gallery: Gallery '
+                     'object (2)>]>',
+                     'm_type': 'Стандартная',
+                     'price': '5999.00',
+                     'quantity': 1,
+                     'size': '4'}
+        assert vars(cart)['cart']['1'] == test_cart

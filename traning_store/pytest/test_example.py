6ee2ed272821991693124d0c10cf345a267bb3cc
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-09-10 14:58:35 +0300

    default articul,code

--- traning_store/pytest/test_example.py
+++ traning_store/pytest/test_example.py
@@ -1,13 +1,23 @@
 from http import HTTPStatus
 
 import pytest
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.management import call_command
+from django.test import RequestFactory
 from django.urls import reverse
 
 
 @pytest.fixture
 def data():
     call_command('loaddata', 'db.json', verbosity=0)
+
+
+@pytest.fixture
+def cart_session_():
+    request = RequestFactory().get('/')
+    middleware = SessionMiddleware(get_response=lambda r: None)
+    middleware.process_request(request)
+    return request.session.save()
 
 
 @pytest.mark.django_db

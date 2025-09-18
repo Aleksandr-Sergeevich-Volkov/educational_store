00c495ed78caec3652d054c07a7536c1f28efa8d
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-09-18 22:05:45 +0300

    add fio

--- traning_store/catalog/forms.py
+++ traning_store/catalog/forms.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Обязательное поле. Введите действующий email.')
+    email = forms.EmailField(max_length=64, label='Почта', help_text='Обязательное поле. Введите действующий email.')
+    first_name = forms.CharField(required=False, max_length=32, label='Имя')
+    last_name = forms.CharField(required=False, max_length=32, label='Фамилия')
 
     def clean_email(self):
         email = self.cleaned_data.get('email', '')
@@ -13,7 +15,17 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        labels = {'username': 'Покупатель', 'email': 'Почта', 'first_name': 'Имя', 'last_name': 'Фамилия',
+                  'password1': 'Пароль',
+                  'password2': 'Повторить пароль'
+                  }
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
 
 
 class UserForm(forms.ModelForm):

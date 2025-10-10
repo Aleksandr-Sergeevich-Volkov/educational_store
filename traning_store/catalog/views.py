1104f66334c8135bf9401a9d0f83aefb5431033a
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-10-10 13:03:04 +0300

    pagination

--- traning_store/catalog/views.py
+++ traning_store/catalog/views.py
@@ -21,7 +21,7 @@ User = get_user_model()
 class ProductListView(FilterView):
     model = Product
     ordering = 'id'
-    paginate_by = 10
+    paginate_by = 5
     template_name = 'product_list.html'
     filterset_class = ProductFilter
     slug_url_kwarg = 'slug'
--- traning_store/traning_store/settings.py
+++ traning_store/traning_store/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-4#xre@vo3c)grje)5kj3rrwt6@1=' \
 CSRF_TRUSTED_ORIGINS = ['https://4505485-volkovaleksandr.twc1.net','https://kompressionnye-chulki24.ru']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0','62.113.36.63','4505485-volkovaleksandr.twc1.net','kompressionnye-chulki24.ru' 
 ]

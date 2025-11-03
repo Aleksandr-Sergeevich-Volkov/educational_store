582500ac8e795e4c9cea737a8faed965e8859604
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-11-04 01:03:11 +0300

    add color in test

--- traning_store/pytest_tests/test_routes.py
+++ traning_store/pytest_tests/test_routes.py
@@ -131,12 +131,12 @@ class TestRoutes(TestCase):
         cart.add(product=self.product1,
                  quantity=1,
                  size=self.size,
-                 color= self.color_black,
+                 color=self.color_black,
                  m_type=self.model_type,
                  images_m=images_m)
 
         expected_cart_item = {
-            'color':  self.color_black,
+            'color': self.color_black.name,
             'images_m': str(images_m),  # Преобразуем QuerySet в строку для сравнения
             'm_type': 'Стандартная',
             'price': '5999.0',

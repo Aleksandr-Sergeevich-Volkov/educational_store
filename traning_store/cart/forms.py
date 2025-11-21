befad83fc873e8315553ca57cb5b167e2c98e06d
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-11-22 00:17:18 +0300

    add color choice

--- traning_store/cart/forms.py
+++ traning_store/cart/forms.py
@@ -5,52 +5,68 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 7)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(label='Кол-во', choices=PRODUCT_QUANTITY_CHOICES,
-                                      coerce=int)
-    update = forms.BooleanField(required=False, initial=False,
-                                widget=forms.HiddenInput)
-    size = forms.ModelChoiceField(label='Размер',
-                                  queryset=Size.objects.none(), required=False,
-                                  )
-    color = forms.ModelChoiceField(label='Цвет',
-                                   queryset=Color.objects.none(),
-                                   required=False,
-                                   )
-    m_type = forms.ModelChoiceField(label='Тип',
-                                    queryset=Model_type.objects.none(),
-                                    required=False,
-                                    )
+    quantity = forms.TypedChoiceField(
+        label='Кол-во',
+        choices=PRODUCT_QUANTITY_CHOICES,
+        coerce=int,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    update = forms.BooleanField(
+        required=False,
+        initial=False,
+        widget=forms.HiddenInput
+    )
+    size = forms.ModelChoiceField(
+        label='Размер',
+        queryset=Size.objects.none(),
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    color = forms.ModelChoiceField(
+        label='Цвет',
+        queryset=Color.objects.none(),
+        required=False,
+        widget=forms.RadioSelect(attrs={'class': 'color-radio'})
+    )
+    m_type = forms.ModelChoiceField(
+        label='Тип',
+        queryset=Model_type.objects.none(),
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
 
     def __init__(self, *args, **kwargs):
         product = kwargs.pop('product', None)
         super().__init__(*args, **kwargs)
 
         if product:
-            # Получаем доступные цвета для этого продукта
             available_colors = product.Color.all()
 
-            # Настраиваем поле цвета в зависимости от количества доступных цветов
             if available_colors.exists():
+                # ВАЖНО: Используем prefetch_related или select_related если нужно
                 self.fields['color'].queryset = available_colors
                 if available_colors.count() == 1:
                     single_color = available_colors.first()
                     self.fields['color'].initial = single_color
                     self.fields['color'].widget = forms.HiddenInput()
                 else:
-                    # Если несколько цветов - показываем выбор
                     self.fields['color'].queryset = available_colors
-                    self.fields['color'].empty_label = "Выберите цвет"
+                    self.fields['color'].empty_label = None
             else:
-                # Если нет доступных цветов - скрываем поле
                 self.fields['color'].widget = forms.HiddenInput()
 
-        if product.brand:
+        if product and product.brand:
             self.fields['m_type'].queryset = Model_type.objects.filter(
                 brand=product.brand
             )
             self.fields['size'].queryset = Size.objects.filter(
                 brand=product.brand
             )
+
+    # ДОБАВЬТЕ ЭТОТ МЕТОД ДЛЯ ЗАГРУЗКИ ОБЪЕКТОВ
+    def get_color_choices(self):
+        """Возвращает цвета с полными объектами"""
+        return [(color.id, color) for color in self.fields['color'].queryset]
 
 
 class DeliveryForm(forms.Form):

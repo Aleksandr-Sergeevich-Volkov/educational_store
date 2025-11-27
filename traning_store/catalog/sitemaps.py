47c30bef49bd858b303853f61bceafa484701275
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-11-27 23:29:10 +0300

    add sitemap.xml

---
+++ traning_store/catalog/sitemaps.py
@@ -0,0 +1,19 @@
+# catalog/sitemaps.py
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+from .models import Product  # ✅ Только модель Product
+
+
+class ProductSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return Product.objects.filter(available=True)
+
+    def lastmod(self, obj):
+        return obj.updated
+
+    def location(self, obj):
+        return reverse('catalog:detail', kwargs={'slug': obj.slug})  # ✅ Товары используют slug
--- traning_store/catalog/urls.py
+++ traning_store/catalog/urls.py
@@ -1,14 +1,21 @@
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 
 from . import views
+from .sitemaps import ProductSitemap
 
 app_name = 'catalog'
+
+sitemaps = {
+    'products': ProductSitemap,
+}
 
 urlpatterns = [
     path('catalog/', views.ProductListView.as_view(), name='catalog'),
     path('catalog/<slug:slug>/',
          views.ProductDetailView.as_view(),
          name='detail'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     path('profile/<username>/',
          views.user_profile, name='profile'),
     path('profile/orders/<int:order_id>/',

e6703d3a6c7fb21d5099ffd10549af88444d01f3
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-12-19 12:39:49 +0300

    rewove sitemap.xml

--- traning_store/catalog/sitemaps.py
+++ traning_store/catalog/sitemaps.py
@@ -1,6 +1,7 @@
 # catalog/sitemaps.py
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from homepage.models import Post
 
 from .models import Product  # ✅ Только модель Product
 
@@ -33,3 +34,22 @@ class ProductSitemap(Sitemap):
 
     def location(self, obj):
         return reverse('catalog:detail', kwargs={'slug': obj.slug})  # ✅ Товары используют slug
+
+
+# НОВЫЙ КЛАСС ДЛЯ СТАТЕЙ
+class ArticleSitemap(Sitemap):
+    changefreq = "monthly"  # Статьи реже обновляются
+    priority = 0.7  # Приоритет ниже, чем у товаров
+    protocol = 'https'
+
+    def items(self):
+        # Возвращаем QuerySet опубликованных статей
+        return Post.objects.all()
+
+    def lastmod(self, obj):
+        # Используем дату последнего изменения
+        return obj.text or obj.title
+
+    def location(self, obj):
+        # Генерируем URL с параметром pk
+        return reverse('homepage:detail_view', kwargs={'pk': obj.pk})

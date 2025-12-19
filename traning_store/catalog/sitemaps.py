3cc0b65feac849b649b69fbcbc629cd55130c8d0
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2025-12-19 12:13:39 +0300

    rewove sitemap.xml

--- traning_store/catalog/sitemaps.py
+++ traning_store/catalog/sitemaps.py
@@ -5,6 +5,21 @@ from django.urls import reverse
 from .models import Product  # ✅ Только модель Product
 
 
+class StaticViewSitemap(Sitemap):
+    priority = 1.0
+    changefreq = 'daily'
+    protocol = 'https'  # Не забываем про HTTPS
+
+    # Список имен URL-путей (name из urls.py)
+    def items(self):
+        return [
+            'homepage:homepage',        # Главная страница
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
 class ProductSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8

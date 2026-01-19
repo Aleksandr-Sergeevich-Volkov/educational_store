ce2a5de59bb458e3ac3afc0f4b840e18d1998380
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2026-01-20 00:06:10 +0300

    add geo fields

--- traning_store/homepage/context_processors.py
+++ traning_store/homepage/context_processors.py
@@ -11,9 +11,13 @@ def city_context(request):
             current_city = City.objects.get(
                 id=request.session['current_city_id']
             )
-        except City.DoesNotExist:
-            pass
-        return {
-            'current_city': current_city,
-            'popular_cities': City.objects.filter(is_popular=True)[:10],
-        }
+        except (City.DoesNotExist, ValueError):
+            # Если город не найден, удаляем из сессии
+            if 'current_city_id' in request.session:
+                del request.session['current_city_id']
+            if 'current_city_name' in request.session:
+                del request.session['current_city_name']
+    return {
+        'current_city': current_city,
+        'popular_cities': City.objects.filter(is_popular=True)[:10],
+    }

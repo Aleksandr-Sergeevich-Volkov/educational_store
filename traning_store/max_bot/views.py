115d045459585dbe431e47e548ba4852798d329b
Александр Волков <volkovaleksandrsergeevich@yandex.ru>
2026-04-07 22:35:50 +0300

    add max_bot

--- traning_store/max_bot/views.py
+++ traning_store/max_bot/views.py
@@ -234,7 +234,7 @@ def show_products_by_category(user_id, category_id):
         buttons = {"buttons": [[{"type": "callback", "text": "🔍 Подробнее", "payload": f"product_{product.id}"}]]}
         if image_url:
             # print(f'user_id:{user_id}, text:{text}, image_url:{image_url},buttons:{buttons}')
-            send_message_with_image(user_id, text, image_url, {"buttons": buttons})
+            send_message_with_image(user_id, text, image_url, buttons)
         else:
             send_message(user_id, text, {"buttons": buttons})
     # Кнопка "Назад"
